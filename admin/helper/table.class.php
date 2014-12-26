07024299b45ba149b98b7bd6f9bf2ec605223e2a
Michał Zagalski <michal@zagalski.pl>
2014-12-26 11:08:41 +0100

    Table class

--- admin/helper/table.class.php
+++ admin/helper/table.class.php
@@ -6,11 +6,21 @@
  * Author: michal[at]zagalski.pl
  */
 
-if ( ! class_exists( '' ) )
+/*
+ * Check whether the WP_List_Table is avaiable, if not require file
+ */
+if ( ! class_exists( 'WP_List_Table' ) )
     require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 
+/*
+ * Table class
+ * WP_List_Table child
+ */
 class Table extends WP_List_Table
-{   
+{  
+    /*
+     * Constructor function
+     */
     function __construct()
     {
         global $status, $page;
@@ -24,19 +34,41 @@ class Table extends WP_List_Table
         );
     }
     
-    function column_defalut()
+    function column_defalut( $item, $column_name )
     {
-        
+        switch( $column_name )
+        {
+            case '':
+            case '':
+                return $item[ $column_name ]
+            default:
+                return print_r( $item, true );
+        }
     }
     
     function column_title()
     {
+        $actions = array(
+            'edit'  => sprintf( '<a href="?page=%s&action=%s&dish=%s">Edytuj</a>', $_REQUEST['page'], 'edit', $item['id'] ),
+            'delete'  => sprintf( '<a href="?page=%s&action=%s&dish=%s">Edytuj</a>', $_REQUEST['delete'], 'edit', $item['id'] ),
+        );
         
+        return sprintf( 
+            '%1$s <span style="color: silver">(id:%2$s)</span>%3$s',
+            count($item['dishes']),
+            $item['id'],
+            $this->row_actions( $actions )
+        );
     }
+    
     
     function column_cb( $item )
     {
-        
+        return sprintf( 
+            '<input type="checkbox" name="%1$s[]" value="%2$s" />',
+            $this->_args['singular'],
+            $item['id']
+        );
     }
     
     function get_columns()
